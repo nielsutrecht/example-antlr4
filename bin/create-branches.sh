9d4b8565d850cfa63bae839ed65fd9016255ef5d
Niels Dommerholt <ndommerholt@gmail.com>
2016-05-30 15:09:12 +0200

    Added script that creates example branches

--- bin/create-branches.sh
+++ bin/create-branches.sh
@@ -1,2 +1,36 @@
 #!/usr/bin/env bash
-git checkout
+
+deleteDirs() {
+    git rm -rf src/main/antlr4/com/nibado/example/antlr4/$1/ 2>/dev/null
+    git rm -rf src/main/java/com/nibado/example/antlr4/$1/ 2>/dev/null
+    git rm -rf src/test/java/com/nibado/example/antlr4/$1/ 2>/dev/null
+}
+
+if [[ `git status --porcelain` ]]; then
+    echo There are changes, add and commit them first.
+    exit 1
+fi
+
+git checkout master
+git branch -Dq example/simple 2>/dev/null
+git branch -Dq example/query 2>/dev/null
+git branch -Dq example/script 2>/dev/null
+
+git checkout -b example/simple
+deleteDirs query
+deleteDirs script
+git commit -m "Deleted query and script dirs"
+
+git checkout master
+git checkout -b example/query
+deleteDirs simple
+deleteDirs script
+git commit -m "Deleted simple and script dirs"
+
+git checkout master
+
+
+
+
+
+
